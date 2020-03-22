1986446fd72ea859a153387c82c33d06e10d312d
Nadhirah <zcabnbi@ucl.ac.uk>
2020-03-22 20:10:36 +0800

    Pulling from git

--- Website/settings.py
+++ Website/settings.py
@@ -18,9 +18,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z2nz_=w0r+&&kc^4+nx6!-p022sxn1)awyg8s$8+s)ez*ak-nm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'system-engineering.herokuapp.com']
 
 # Application definition
 
@@ -188,5 +188,3 @@ SOCIAL_AUTH_FACEBOOK_EXTRA_DATA = [
     ('picture', 'picture'),
     ('link', 'profile_url'),
 ]
-
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
